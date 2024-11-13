864ef109c867ff383f926c4133be1073f5d6b6a1
miloszmat123 <myyylek@gmail.com>
2024-11-13 12:53:42 +0100

    add new values to enum

--- com/bartoszwalter/students/taxes/FeePercentValues.java
+++ com/bartoszwalter/students/taxes/FeePercentValues.java
@@ -1,5 +1,7 @@
 package com.bartoszwalter.students.taxes;
 
+import java.math.BigDecimal;
+
 public enum FeePercentValues {
     SOCIAL_SECURITY(9.76),
     HEALTH_INSURANCE(1.5),
@@ -7,15 +9,17 @@ public enum FeePercentValues {
     CURRENT_SOCIAL_HEALTH_INSURANCE(9),
     PREVIOUS_SOCIAL_HEALTH_INSURANCE(7.75),
     ADVANCE_TAX(18),
-    DEDUCTABLE_EXPENSES(20);
+    DEDUCTIBLE_EXPENSES_CIVIL(20),
+    DEDUCTIBLE_EXPENSES_EMPLOYMENT(111.25),
+    TAX_FREE_INCOME(46.33);
 
-    private final double percentage;
+    private final BigDecimal value;
 
-    FeePercentValues(double percentage) {
-        this.percentage = percentage;
+    FeePercentValues(double value) {
+        this.value = BigDecimal.valueOf(value);
     }
 
-    public double getPercentage() {
-        return percentage;
+    public BigDecimal getValue() {
+        return value;
     }
 }
