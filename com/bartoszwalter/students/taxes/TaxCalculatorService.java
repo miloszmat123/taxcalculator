ce45a433cb4edd0fbe111ac694375fa19e75885f
miloszmat123 <myyylek@gmail.com>
2024-11-13 12:53:34 +0100

    change double to BigDecimal

--- com/bartoszwalter/students/taxes/TaxCalculatorService.java
+++ com/bartoszwalter/students/taxes/TaxCalculatorService.java
@@ -1,69 +1,83 @@
 package com.bartoszwalter.students.taxes;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class TaxCalculatorService {
-    private double preTaxIncome;
+    private BigDecimal preTaxIncome;
 
-
-    public TaxCalculatorService(double preTaxIncome) {
+    public TaxCalculatorService(BigDecimal preTaxIncome) {
         this.preTaxIncome = preTaxIncome;
     }
 
-    public double getSocialSecurityFee() {
-        return (preTaxIncome * FeePercentValues.SOCIAL_SECURITY.getPercentage()) / 100;
+    public BigDecimal getSocialSecurityFee() {
+        return preTaxIncome.multiply(FeePercentValues.SOCIAL_SECURITY.getValue()).divide(BigDecimal.valueOf(100), RoundingMode.HALF_UP);
     }
 
-    public double getHealthInsuranceFee() {
-        return (preTaxIncome * FeePercentValues.HEALTH_INSURANCE.getPercentage()) / 100;
+    public BigDecimal getHealthInsuranceFee() {
+        return preTaxIncome.multiply(FeePercentValues.HEALTH_INSURANCE.getValue()).divide(BigDecimal.valueOf(100), RoundingMode.HALF_UP);
     }
 
-    public double getSicknessInsuranceFee() {
-        return (preTaxIncome * FeePercentValues.SICKNESS_INSURANCE.getPercentage()) / 100;
+    public BigDecimal getSicknessInsuranceFee() {
+        return preTaxIncome.multiply(FeePercentValues.SICKNESS_INSURANCE.getValue()).divide(BigDecimal.valueOf(100), RoundingMode.HALF_UP);
     }
 
-    public double getCurrentSocialHealthInsuranceFee() {
-        return (preTaxIncome * FeePercentValues.CURRENT_SOCIAL_HEALTH_INSURANCE.getPercentage()) / 100;
+    public BigDecimal getCurrentSocialHealthInsuranceFee() {
+        return preTaxIncome.multiply(FeePercentValues.CURRENT_SOCIAL_HEALTH_INSURANCE.getValue()).divide(BigDecimal.valueOf(100), RoundingMode.HALF_UP);
     }
 
-    public double getPreviousSocialHealthInsuranceFee() {
-        return (preTaxIncome * FeePercentValues.PREVIOUS_SOCIAL_HEALTH_INSURANCE.getPercentage()) / 100;
+    public BigDecimal getPreviousSocialHealthInsuranceFee() {
+        return preTaxIncome.multiply(FeePercentValues.PREVIOUS_SOCIAL_HEALTH_INSURANCE.getValue()).divide(BigDecimal.valueOf(100), RoundingMode.HALF_UP);
     }
 
-    public double getTaxFreeIncome() {
-        return 0;
-    } //no taxFreeIncome info in previous code
 
-    public double getTaxDeductibleExpenses() {
-        return 0;
+    public BigDecimal getTaxFreeIncome() {
+        return BigDecimal.ZERO;
     }
 
-    public double getAdvanceTax() {
-        return (preTaxIncome * FeePercentValues.ADVANCE_TAX.getPercentage()) / 100;
+
+    public BigDecimal getTaxDeductibleExpenses() {
+        return BigDecimal.ZERO;
     }
 
-    public double getAdvanceTaxPaid() {
-        return getPreviousSocialHealthInsuranceFee() - getTaxFreeIncome();
+
+    public BigDecimal getAdvanceTax() {
+        return preTaxIncome.multiply(FeePercentValues.ADVANCE_TAX.getValue()).divide(BigDecimal.valueOf(100), RoundingMode.HALF_UP);
     }
 
-    public double getNetIncome() {
-        return preTaxIncome - ((getSocialSecurityFee() + getHealthInsuranceFee() + getSicknessInsuranceFee())
-                + getCurrentSocialHealthInsuranceFee() + getAdvanceTaxPaid());
+
+    public BigDecimal getAdvanceTaxPaid() {
+        return getPreviousSocialHealthInsuranceFee().subtract(getTaxFreeIncome());
     }
 
-    public double getIncomeAfterTax() {
-        return (preTaxIncome - getSocialSecurityFee() - getHealthInsuranceFee() - getSicknessInsuranceFee());
+
+    public BigDecimal getNetIncome() {
+        BigDecimal totalDeductions = getSocialSecurityFee()
+                .add(getHealthInsuranceFee())
+                .add(getSicknessInsuranceFee())
+                .add(getCurrentSocialHealthInsuranceFee())
+                .add(getAdvanceTaxPaid());
+        return preTaxIncome.subtract(totalDeductions);
     }
 
-    public double getTaxedIncome() {
-        return preTaxIncome - getTaxDeductibleExpenses();
+    public BigDecimal getIncomeAfterTax() {
+        return preTaxIncome.subtract(getSocialSecurityFee())
+                .subtract(getHealthInsuranceFee())
+                .subtract(getSicknessInsuranceFee());
     }
 
-    public double getTaxPaid() {
-        return getAdvanceTax() - getTaxFreeIncome();
+
+    public BigDecimal getTaxedIncome() {
+        return preTaxIncome.subtract(getTaxDeductibleExpenses());
     }
 
-    public double getPreTaxIncome() {
+
+    public BigDecimal getTaxPaid() {
+        return getAdvanceTax().subtract(getTaxFreeIncome());
+    }
+
+
+    public BigDecimal getPreTaxIncome() {
         return preTaxIncome;
     }
-
-
 }
