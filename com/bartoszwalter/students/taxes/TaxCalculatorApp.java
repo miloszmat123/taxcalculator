d721f9feee19fa0692d9ac14ddcabd516b3ff4f9
miloszmat123 <myyylek@gmail.com>
2024-11-13 12:53:19 +0100

    refactor printing and reader

--- com/bartoszwalter/students/taxes/TaxCalculatorApp.java
+++ com/bartoszwalter/students/taxes/TaxCalculatorApp.java
@@ -3,30 +3,17 @@ package com.bartoszwalter.students.taxes;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
 class TaxCalculatorApp {
-    private static final DecimalFormat twoDecimalFormat = new DecimalFormat("0.00");
-    private static final DecimalFormat noDecimalFormat = new DecimalFormat("#");
 
     public static void main(String[] args) {
         char contractType;
-        double income;
-        try {
-            InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        BigDecimal income;
 
-            System.out.print("Enter income: ");
-            income = Double.parseDouble(bufferedReader.readLine());
-
-            System.out.print("Contract Type: (E)mployment, (C)ivil: ");
-            contractType = bufferedReader.readLine().charAt(0);
-
-        } catch (IOException | NumberFormatException ex)
-        {
-            System.out.println("Incorrect input data! Error: " + ex);
-            return;
-        }
+        income = readIncome();
+        contractType = readContractType();
 
         if (contractType == 'E') {
             System.out.println("Income: " + income);
@@ -43,38 +30,70 @@ class TaxCalculatorApp {
         }
     }
 
+    public static BigDecimal readIncome() {
+        BigDecimal income = BigDecimal.ZERO;
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+
+            System.out.print("Enter income: ");
+            income = new BigDecimal(br.readLine());
+
+        } catch (IOException ex) {
+            System.out.println("Incorrect");
+            System.err.println(ex);
+        }
+        return income;
+    }
+
+    public static char readContractType() {
+        char contractType = ' ';
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+
+            System.out.print("Contract Type: (E)mployment, (C)ivil: ");
+            contractType = br.readLine().charAt(0);
+
+        } catch (IOException ex) {
+            System.out.println("Incorrect");
+            System.err.println(ex);
+        }
+        return contractType;
+    }
+
     public static void printTaxInformation(TaxCalculatorService taxCalculatorService) {
         System.out.println("Social security tax: "
-				+ twoDecimalFormat.format(taxCalculatorService.getSocialSecurityFee()));
+                + taxCalculatorService.getSocialSecurityFee());
 
         System.out.println("Health social security tax: "
-				+ twoDecimalFormat.format(taxCalculatorService.getHealthInsuranceFee()));
+                + taxCalculatorService.getHealthInsuranceFee());
 
         System.out.println("Sickness social security tax: "
-				+ twoDecimalFormat.format(taxCalculatorService.getSicknessInsuranceFee()));
+                + taxCalculatorService.getSicknessInsuranceFee());
 
-        System.out.println("Income basis for health social security: "+ taxCalculatorService.getIncomeAfterTax());
+        System.out.println("Income basis for health social security: " + taxCalculatorService.getIncomeAfterTax());
 
         System.out.println("Health social security tax: 9% = "
-				+ twoDecimalFormat.format(taxCalculatorService.getCurrentSocialHealthInsuranceFee())
-				+ " 7,75% = " + twoDecimalFormat.format(taxCalculatorService.getPreviousSocialHealthInsuranceFee()));
+                + (taxCalculatorService.getCurrentSocialHealthInsuranceFee())
+                + " 7,75% = " + taxCalculatorService.getPreviousSocialHealthInsuranceFee());
 
         System.out.println("Tax deductible expenses: "
-				+ taxCalculatorService.getTaxDeductibleExpenses());
+                + taxCalculatorService.getTaxDeductibleExpenses());
 
         System.out.println("Income: " + taxCalculatorService.getTaxedIncome()
-				+ " rounded: " + noDecimalFormat.format(taxCalculatorService.getTaxedIncome()));
+                + " rounded: " + taxCalculatorService.getTaxedIncome());
 
         System.out.println("Advance tax 18%: " + taxCalculatorService.getAdvanceTax());
 
         System.out.println("Tax free income: " + taxCalculatorService.getTaxFreeIncome());
 
-        System.out.println("Reduced tax: " + twoDecimalFormat.format(taxCalculatorService.getTaxPaid()));
+        System.out.println("Reduced tax: " + taxCalculatorService.getTaxPaid());
 
-        System.out.println("Advance tax paid: " + twoDecimalFormat.format(taxCalculatorService.getAdvanceTaxPaid())
-				+ " rounded: " + noDecimalFormat.format(taxCalculatorService.getAdvanceTaxPaid()));
+        System.out.println("Advance tax paid: " + (taxCalculatorService.getAdvanceTaxPaid())
+                + " rounded: " + taxCalculatorService.getAdvanceTaxPaid());
 
-        System.out.println("Net income: "+ taxCalculatorService.getNetIncome());
+        System.out.println("Net income: " + taxCalculatorService.getNetIncome());
     }
 }
 
